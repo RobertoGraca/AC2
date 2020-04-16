123c34309facbac4eecbf897ba09a7e86d42c716
Whitesnake <robertograca@ua.pt>
2020-04-16 10:44:45 +0100

    Contagem de tempo

--- Guiao4/parte2/Ex2.c
+++ Guiao4/parte2/Ex2.c
@@ -9,28 +9,29 @@ void delay(unsigned int ms){
     }
 }
 
-void send2displays(){
-    unsigned char value;
+void send2displays( unsigned char value){
+   
     unsigned int i=0;
     static const char display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F,
                                           0x66, 0x6D, 0x7D, 0x07, 
                                           0x7F, 0x6F, 0x77, 0x7C, 
                                           0x39, 0x5E, 0x79, 0x71};
-    while(i<1000000){
+    while(1){
         LATDbits.LATD5 = 1;
         LATDbits.LATD6 = 0;
         LATB = (LATB & 0x00FF) | (display7Scodes[(value >> 4)] << 8);
         delay(20);
 
-        LATD = LATD ^ 0x0060;
+        LATDbits.LATD5 = 0;
+        LATDbits.LATD6 = 1;
         LATB = (LATB & 0x00FF) | (display7Scodes[value & 0x0F] << 8);
         delay(20);
-        i++;
     }
 }
 
 int main(void){
     unsigned int num;
+    unsigned char value=0;
     static const char display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F,
                                           0x66, 0x6D, 0x7D, 0x07, 
                                           0x7F, 0x6F, 0x77, 0x7C, 
@@ -42,7 +43,9 @@ int main(void){
     TRISB = TRISB & 0x00FF;
     TRISD = TRISD & 0xFF9F;
 
-    send2displays();
-
+    while(1){
+        send2displays(value++);
+        delay(200);
+    }
     return 0;
 }
